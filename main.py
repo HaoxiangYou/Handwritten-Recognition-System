425b78f7ff156e2e4b9ee0a5fd533f8f11b7b13e
Haoxiang You <youhaox@seas.upenn.edu>
2022-04-24 16:35:10 -0400

    can set is_vis for test

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from handwritten_recognition_system import Handwrittten_recognition_system
 from utils.handwritten_dataset_utils import view_word
 from utils.Initial_and_Transition_matrix_generator import index_to_alphabet
 
-def eval(images, path, label, alg="viterbi"):
+def eval(images, path, label, alg="viterbi", is_vis=True):
     handwritten_recogonition_predictor = Handwrittten_recognition_system(path, alg)
 
     n = images.shape[0]
@@ -20,8 +20,9 @@ def eval(images, path, label, alg="viterbi"):
 
         if predict_vocabulary == label:
             succes_predict +=1
-
-        view_word(images[i,:], predict_vocabulary, pause_time = 0.1)
+        
+        if is_vis:
+            view_word(images[i,:], predict_vocabulary, pause_time = 0.1)
 
     return succes_predict/n
 
@@ -35,6 +36,7 @@ def main():
                         default='trained_model/transition_matrix_and_initial_distribution')
     parser.add_argument('--observer_path', type=str,
                         default="trained_model/observer/model_cnn.pt")
+    parser.add_argument('--is_vis', default=False, action='store_true')
 
     args = parser.parse_args()
     
@@ -45,6 +47,7 @@ def main():
     transition_matrices_path = os.path.join(distribution_dir, "transition_matrix.npy")
     init_distribution_path = os.path.join(distribution_dir, "initial_distribution.npy")
     observer_path = args.observer_path
+    is_vis = args.is_vis
 
     images = np.load(images_path)
 
@@ -55,8 +58,8 @@ def main():
     algs = ["max_likelihood", "bayes_filter", "smooth", "viterbi"]
 
     for alg in algs:
-        accuracy = eval(images, path, vocabulary_label, alg=alg)
-        print("The accuarcy of predict {} by using {} algorithmn is\n {}".format(vocabulary_label, alg, accuracy))
+        accuracy = eval(images, path, vocabulary_label, alg=alg, is_vis=is_vis)
+        print("The accuracy of predict {} by using {} algorithmn is\n {}".format(vocabulary_label, alg, accuracy))
 
 
 if __name__ == "__main__":
