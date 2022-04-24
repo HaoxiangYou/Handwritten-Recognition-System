04adc9300b406567aa4ff3818e2a6a62a4dd2fb0
Haoxiang You <youhaox@seas.upenn.edu>
2022-04-24 16:25:59 -0400

    accuracy analysis to algs

--- main.py
+++ main.py
@@ -5,20 +5,32 @@ from handwritten_recognition_system import Handwrittten_recognition_system
 from utils.handwritten_dataset_utils import view_word
 from utils.Initial_and_Transition_matrix_generator import index_to_alphabet
 
-def eval(images, path, alg="viterbi"):
+def eval(images, path, label, alg="viterbi"):
     handwritten_recogonition_predictor = Handwrittten_recognition_system(path, alg)
 
-    for i in range(images.shape[0]):
+    n = images.shape[0]
+
+    succes_predict = 0
+
+    for i in range(n):
         handwritten_recogonition_predictor.load_image(images[i,:])
         predict_states = handwritten_recogonition_predictor.make_prediction()
         predict_letters = [index_to_alphabet[x] for x in predict_states]
         predict_vocabulary =  "".join(predict_letters)
+
+        if predict_vocabulary == label:
+            succes_predict +=1
+
         view_word(images[i,:], predict_vocabulary, pause_time = 0.1)
+
+    return succes_predict/n
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--images_path',type=str, 
-                        default='dataset/handwritten_alphabets/words/robotics.npy')
+    parser.add_argument('--images_dir',type=str, 
+                        default='dataset/handwritten_alphabets/words')
+    parser.add_argument('--vocabulary', type=str,
+                        default='robotics')
     parser.add_argument('--distribution_dir', type=str,
                         default='trained_model/transition_matrix_and_initial_distribution')
     parser.add_argument('--observer_path', type=str,
@@ -26,7 +38,9 @@ def main():
 
     args = parser.parse_args()
     
-    images_path = args.images_path
+    images_dir= args.images_dir
+    vocabulary_label = args.vocabulary
+    images_path = os.path.join(images_dir, vocabulary_label + ".npy")
     distribution_dir = args.distribution_dir
     transition_matrices_path = os.path.join(distribution_dir, "transition_matrix.npy")
     init_distribution_path = os.path.join(distribution_dir, "initial_distribution.npy")
@@ -38,7 +52,11 @@ def main():
             "transition": transition_matrices_path,
             "observer":observer_path}
 
-    eval(images, path)
+    algs = ["max_likelihood", "bayes_filter", "smooth", "viterbi"]
+
+    for alg in algs:
+        accuracy = eval(images, path, vocabulary_label, alg=alg)
+        print("The accuarcy of predict {} by using {} algorithmn is\n {}".format(vocabulary_label, alg, accuracy))
 
 
 if __name__ == "__main__":
